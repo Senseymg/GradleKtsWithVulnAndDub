83c2cd01e85550019aa6e97e3727f3b7ff63d080
SenseyMG <33028581+Senseymg@users.noreply.github.com>
2024-12-19 09:56:04 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "groovy.test"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web:3.4.0")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.7.13")
+	implementation("org.springframework.boot:spring-boot-starter-groovy-templates")
+	implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+	implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
